f1da6953ca89616624d663d0b2fbc699282eee6a
atacan <atacandur@icloud.com>
2024-11-03 16:01:13 +0100

    cli

--- scripts/downloadOpenAPIDocument.swift
+++ scripts/downloadOpenAPIDocument.swift
@@ -1,10 +1,28 @@
 import Foundation
 
+func runCommand(_ command: String) throws {
+    let task = Process()
+    let pipe = Pipe()
+
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.executableURL = URL(fileURLWithPath: "/bin/zsh")
+    task.standardInput = nil
+
+    try task.run()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+
+    print(#function, command, "\n", output)
+}
+
 let fileURL = URL(
     string: "https://raw.githubusercontent.com/AssemblyAI/assemblyai-api-spec/refs/heads/main/openapi.yml"
 )!
 let destinationPaths = [
-    "./Sources/AssemblyAI_AHC/openapi.yaml",
+    "./Sources/AssemblyAI_AHC/openapi.yaml"
 ]
 
 func downloadFile(from fileURL: URL, to destinationPaths: [String]) async throws {
@@ -14,9 +32,12 @@ func downloadFile(from fileURL: URL, to destinationPaths: [String]) async throws
     var fileContent = try String(data: fileData, encoding: .utf8)!
 
     // Apply content modifications
-    fileContent = fileContent
-                    // Fix overflowing integers
-                    .replacingOccurrences(of: "9223372036854776000", with: "922337203685477600")
+    fileContent =
+        fileContent
+        // Fix overflowing integers
+        .replacingOccurrences(of: "9223372036854776000", with: "922337203685477600")
+        // Remove the null type
+        // .replacingOccurrences(of: #"  - type: "null""#, with: "nullable: true")
 
     // Save to each destination path
     try await withThrowingTaskGroup(of: Void.self) { group in
@@ -27,6 +48,8 @@ func downloadFile(from fileURL: URL, to destinationPaths: [String]) async throws
                     atomically: true,
                     encoding: .utf8
                 )
+                // Downgrade to 3.0.0
+                // try runCommand("openapi-down-convert --input \(destinationPath) --output \(destinationPath)")
                 print("Successfully downloaded and saved file to: \(destinationPath)")
             }
         }
